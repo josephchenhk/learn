7f1f9edcd44c6de62624cb17dec01a0a3e517e75
josephchenhk <josephchenhk@gmail.com>
2019-10-24 18:26:58 +0800

    updated thread tutorial

---
+++ Python/python3-cookbook/chp12并发编程/12.7 创建一个线程池/12.7.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 10/24/2019 5:32 PM
+# @Author  : Joseph Chen
+# @Email   : joseph.chen@magnumwm.com
+# @FileName: 12.7.py
+# @Software: PyCharm
+
+"""
+12.7 创建一个线程池
+
+
+"""
