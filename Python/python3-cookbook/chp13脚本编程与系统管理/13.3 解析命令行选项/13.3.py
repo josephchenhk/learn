0ccbfad76915714c40e3483e9d411fe89a780050
josephchenhk <josephchenhk@gmail.com>
2019-10-25 17:48:12 +0800

    added shell programing

---
+++ Python/python3-cookbook/chp13脚本编程与系统管理/13.3 解析命令行选项/13.3.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time    : 10/25/2019 9:18 AM
+# @Author  : Joseph Chen
+# @Email   : joseph.chen@magnumwm.com
+# @FileName: 13.3.py
+# @Software: PyCharm
+
+"""
+13.3 解析命令行选项
+
+你的程序如何能够解析命令行选项（位于sys.argv中）
+- argparse 模块可被用来解析命令行选项
+"""
