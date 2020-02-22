601fa97037cbbfd656594545812b869964587bf2
josephchenhk <josephchenhk@gmail.com>
2020-02-22 21:35:12 +0800

    added doctest

---
+++ Python/doctest_example/temp.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @Time    : 22/2/2020 9:32 PM
+# @Author  : Joseph Chen
+# @Email   : josephchenhk@gmail.com
+# @FileName: temp.py
+# @Software: PyCharm
+
+
+"""
+    邮件doctest部分，选择"Run Doctest in temp"
+
+    >>> print(3.14)
+    3.14
+"""
+
+if __name__ == '__main__':
+    pass
