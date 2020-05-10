7b2f2471e4eb3488dbc78a741f39201634f00293
josephchenhk <josephchenhk@gmail.com>
2020-05-10 09:16:59 +0800

    [update] function annotations

--- Python/python3-cookbook/chp7 函数/7.3 给函数参数增加元信息/7.3 给函数参数增加元信息.py
+++ Python/python3-cookbook/chp7 函数/7.3 给函数参数增加元信息/7.3 给函数参数增加元信息.py
@@ -2,7 +2,7 @@
 # @Time    : 7/23/2019 7:23 PM
 # @Author  : Joseph Chen
 # @Email   : joseph.chen@magnumwm.com
-# @FileName: 7.3.py
+# @FileName: 7.3 给函数参数增加元信息.py
 # @Software: PyCharm
 """
 7.3 给函数参数增加元信息
@@ -10,4 +10,9 @@
 使用函数参数注解是一个很好的办法，它能提示程序员应该怎样正确使用这个函数
 """
 def add(x:int, y:int) -> int:
+    """simple add function"""
     return x + y
+
+if __name__=="__main__":
+    print(help(add))
+    print(add.__annotations__)
