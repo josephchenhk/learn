635c57f0cf9e555f8ad16862f337362c7238524e
josephchenhk <josephchenhk@gmail.com>
2020-05-25 18:01:40 +0800

    [update] decorator timethis

--- Python/python3-cookbook/chp9 元编程/9.1 在函数上添加包装器/9.1 在函数上添加包装器.py
+++ Python/python3-cookbook/chp9 元编程/9.1 在函数上添加包装器/9.1 在函数上添加包装器.py
@@ -2,7 +2,7 @@
 # @Time    : 7/24/2019 5:32 PM
 # @Author  : Joseph Chen
 # @Email   : josephchenhk@gmail.com
-# @FileName: 9.1.py
+# @FileName: 9.1 在函数上添加包装器.py
 # @Software: PyCharm
 """
 9.1 在函数上添加包装器
@@ -25,10 +25,21 @@ def timethis(func):
         return result
     return wrapper
 
-@timethis
 def countdown(n):
     """Counts down"""
     while n > 0:
         n -= 1
 
+print("""函数可以作为参数传递进另一个函数：""")
+countdown = timethis(countdown)
 countdown(100000)
+
+print("""以上方法和下面这样的装饰器定义是等价的：""")
+# 等价于 countdown2 = timethis(countdown2)
+@timethis
+def countdown2(n):
+    """Counts down"""
+    while n > 0:
+        n -= 1
+
+countdown2(100000)
