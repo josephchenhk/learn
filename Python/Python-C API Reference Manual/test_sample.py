cc10375d67431e23d396c5f90dfa6d6f89322ed9
josephchenhk <josephchenhk@gmail.com>
2020-06-15 17:53:36 +0800

    [add] Python type

--- Python/Python-C API Reference Manual/test_sample.py
+++ Python/Python-C API Reference Manual/test_sample.py
@@ -8,6 +8,30 @@
 from sample import *
 # spam_system, print_function, add_function, func1_function, func2_function, func3
 
+def test_type():
+    class A(object):
+        pass
+
+    a = A()
+
+    print(type(a))                   # <class '__main__.test_type.<locals>.A'> 实例对象a的类型是A
+    print(isinstance(a, A))          # True  a是A的实例对象
+    print(isinstance(a, object))     # True  a同时也是object的实例对象
+    print(isinstance(a, type))       # False a是一个实例对象，所以它不属于类型对象
+
+    print(type(A))                   # <class 'type'>    类型对象A的类型是<class 'type'>
+    print(A.__base__)                # <class 'object'>  类型对象A的父类是object，即它的base是<class 'object'>
+    print(isinstance(A, object))     # True  A既是object的实例 ？？
+    print(isinstance(A, type))       # True  A也是type的实例 ？？
+
+    print(type(object))              # <class 'type'> 类型对象object的类型是<class 'type'>
+    print(isinstance(object, type))  # True           类型对象object是type的实例（鸡生蛋）
+
+    print(type(type))                # <class 'type'> 类型对象type的类型也是<class 'type'>
+    print(isinstance(type, object))  # True           类型对象type也是object的实例（蛋生鸡）
+
+print(test_type())
+
 # print(spam_system("ls -l"))
 # print(print_function())
 # print(add_function(2, 4))
@@ -15,5 +39,5 @@ from sample import *
 # print(func1_function(1, "hello", (3,4), 50))
 # print(func2_function(1))
 # print(func2_function(3, state="happy", action="sing", type="red bird"))
-print(func3(int, (1.234, )))
-print(func3(eval, ("2+3.1", )))
+# print(func3(int, (1.234, )))
+# print(func3(eval, ("2+3.1", )))
