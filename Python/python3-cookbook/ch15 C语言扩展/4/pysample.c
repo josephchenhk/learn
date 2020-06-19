a9107e51194f3e20f5c5c7b73a317a2fb8763d2c
josephchenhk <josephchenhk@gmail.com>
2020-06-19 19:43:49 +0800

    [add] py_capsule

--- Python/python3-cookbook/ch15 C语言扩展/4/pysample.c
+++ Python/python3-cookbook/ch15 C语言扩展/4/pysample.c
@@ -1,10 +1,9 @@
 #include "Python.h"
+#include "sample.h"
 
 /*
 输入一个Python iterable，在c语言里将之处理每一个元素（打印出来）
-
 */
-
 static PyObject *py_consume_iterable(PyObject *self, PyObject *args) {
     PyObject *obj;
     PyObject *iter;
@@ -34,9 +33,74 @@ static PyObject *py_consume_iterable(PyObject *self, PyObject *args) {
     return Py_BuildValue("");
 }
 
+
+
+///* 定义一个Point结构体 */
+//typedef struct Point {
+//    double x,y;
+//} Point;
+
+
+/* Destructor function for points */
+static void del_Point(PyObject *obj) {
+    free(PyCapsule_GetPointer(obj,"Point"));
+}
+
+
+/* 将一个(PyObject *)转换成Point */
+static Point *PyPoint_AsPoint(PyObject *obj) {
+    return (Point *) PyCapsule_GetPointer(obj, "Point");
+}
+
+
+/* 将一个(Point *)转换成 (PyObject *) */
+static PyObject *PyPoint_FromPoint(Point *p, int must_free) {
+    return PyCapsule_New(p, "Point", must_free ? del_Point : NULL);
+}
+
+
+/* Create a new Point object */
+static PyObject *py_Point(PyObject *self, PyObject *args) {
+    Point *p;
+    double x,y;
+    if (!PyArg_ParseTuple(args,"dd",&x,&y)) {
+        return NULL;
+    }
+    p = (Point *) malloc(sizeof(Point));
+    p->x = x;
+    p->y = y;
+    return PyPoint_FromPoint(p, 1);
+}
+
+static PyObject *py_distance(PyObject *self, PyObject *args) {
+    Point *p1, *p2;
+    PyObject *py_p1, *py_p2;
+    double result;
+
+    if (!PyArg_ParseTuple(args,"OO",&py_p1, &py_p2)) {
+    return NULL;
+    }
+    if (!(p1 = PyPoint_AsPoint(py_p1))) {
+    return NULL;
+    }
+    if (!(p2 = PyPoint_AsPoint(py_p2))) {
+    return NULL;
+    }
+    result = distance(p1,p2);
+    return Py_BuildValue("d", result);
+}
+
+
+
+
+
+
+
 /* Module method table */
 static PyMethodDef SampleMethods[] = {
     {"consume_iterable",  py_consume_iterable, METH_VARARGS, "Print Python iterable"},
+    {"distance",  py_distance, METH_VARARGS, "Distance"},
+    {"Point",  py_Point, METH_VARARGS, "Point"},
     { NULL, NULL, 0, NULL}
 };
 
--- Python/python3-cookbook/ch15 C语言扩展/4/test_sample4.py
+++ Python/python3-cookbook/ch15 C语言扩展/4/test_sample4.py
@@ -9,3 +9,11 @@ import sample
 
 data = [1, 3, 4]
 sample.consume_iterable(data)
+
+
+p1 = sample.Point(2,3)
+p2 = sample.Point(4,5)
+
+print(p1)
+print(p2)
+print(sample.distance(p1,p2))
