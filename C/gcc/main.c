a9127436e7a4c265a6ab519dc089a18c8d491a5f
josephchenhk <josephchenhk@gmail.com>
2020-06-21 09:27:05 +0800

    [add] unsigned int

--- C/gcc/main.c
+++ C/gcc/main.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
+#include <limits.h>
+#include <math.h>
 
 int add(int a, int b);
 int main()
 {
     printf("%d\n", add(1,2));
+
+    printf("int 存储大小 : %lu (2^(4*8)=2^32=%f)\n", sizeof(int), pow(2, sizeof(int)*8));
+
+
     return 0;
 }
