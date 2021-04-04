ee542435413a61c18e9482021acc6ddaddcd0c06
josephchenhk <josephchenhk@gmail.com>
2021-04-04 11:42:02 +0800

    [add] save pd without index col

--- Python/scripts/data_processing.py
+++ Python/scripts/data_processing.py
@@ -13,16 +13,11 @@ DATA_PATH = "/Users/joseph/Dropbox/code/stat-arb/data/capital_distribution"
 stocks = [f for f in os.listdir(DATA_PATH) if not f.startswith(".")]
 
 for stock in stocks:
-    timestamps = sorted([f.replace(".csv","") for f in os.listdir(f"{DATA_PATH}/{stock}")])
-    dates = set(t.split(" ")[0] for t in timestamps)
-    assert len(dates)==1, "data not correct"
-    date = list(dates)[0]
-    for idx, timestamp in enumerate(timestamps):
-        df = pd.read_csv(f"{DATA_PATH}/{stock}/{timestamp}.csv")
-        if idx==0:
-            capital_dist = df.copy()
-        else:
-            capital_dist = pd.concat([capital_dist, df])
-    capital_dist.to_csv(f"{DATA_PATH}/{stock}/{date}.csv")
-    print(f"Save {stock}")
-print("Done.")
+    dates = sorted([f.replace(".csv","") for f in os.listdir(f"{DATA_PATH}/{stock}") if not f.startswith(".")])
+
+    for idx, date in enumerate(dates):
+        df = pd.read_csv(f"{DATA_PATH}/{stock}/{date}.csv")
+        if 'Unnamed: 0' in df.columns:
+            df = df.drop(columns=['Unnamed: 0'])
+            df.to_csv(f"{DATA_PATH}/{stock}/{date}.csv", index=False)
+
