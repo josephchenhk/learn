4afba4da960d1fe87fec94692f1a81c6c37f3149
josephchenhk <josephchenhk@gmail.com>
2021-04-20 10:16:42 +0800

    [add] list and dict as default param

--- Python/python_ref/test.py
+++ Python/python_ref/test.py
@@ -6,17 +6,42 @@
 # @Software: PyCharm
 
 class Test:
-    def __init__(self, a={}):
+    def __init__(self, a=dict()):
         self.a = a
 
-    def update(self):
-        self.a[1] = 2
+    def add(self, k, v):
+        self.a[k] = v
 
+# class Test:
+#     def __init__(self, a=list()):
+#         self.a = a
+#
+#     def add(self):
+#         self.a.append(2)
 
-t = Test()
-print(t.a)
+# class Test:
+#     def __init__(self, a=0):
+#         self.a = a
+#
+#     def add(self):
+#         self.a += 1
 
-t.update()
+t1 = Test()
+print(t1.a)
+
+t1.add(1,2)
 
 t2 = Test()
 print(t2.a)
+
+t2.add(3,4)
+
+t3 = Test()
+print(t3.a)
+
+"""
+Ref: https://blog.csdn.net/dpengwang/article/details/100128420
+
+当list ， dict等可变类型作为默认参数时，只会在定义函数的时候执行一次，有点像静态变量，定义一次后面一直使用。在之后使用函数的过程中，如果没有对默认
+参数进行赋值，那么将会一直使用这个初始的变量。
+"""
