11adf69057b1b099a19a29d4805223fdec0b56a5
josephchenhk <josephchenhk@gmail.com>
2022-05-28 08:44:53 +0800

    Add confidence level

--- Python/football_analysis/test.py
+++ Python/football_analysis/test.py
@@ -37,8 +37,8 @@ def drop_unnamed_cols(df: pd.DataFrame) ->pd.DataFrame:
     return df
 
 df = None
-year = 2021
-for month in range(1, 12+1):
+year = 2022
+for month in range(3, 5+1):
     sheet_name = f"{year}.{month:02d}"
     print(sheet_name)
 
@@ -56,17 +56,26 @@ for month in range(1, 12+1):
 
 df["玩法分类"] = df["玩法"].apply(bet_type)
 df["投入"] = df["投入"].astype(float)
+df["水位"] = df["水位"].astype(float)
+df["信心指数"] = df["信心指数"].astype(float)
+df["期望回报"] = df["水位"] * df["信心指数"]
 
 path = f"{year}年微博比赛结果分析.xlsx"
 writer = pd.ExcelWriter(path, engine = 'xlsxwriter')
 df.to_excel(writer, sheet_name="汇总")
 
-res = df.groupby(['联赛']).sum()
+res1 = df[["投入", "盈利", "联赛"]].groupby(['联赛']).sum()
+res2 = df[["水位", "信心指数", "期望回报", "联赛"]].groupby(['联赛']).mean()
+res3 = df[["投入", "联赛"]].groupby(['联赛']).count().rename(columns={"投入": "次数"})
+res = res1.join(res2).join(res3)
 res["盈利比例"] = res["盈利"] / res["投入"]
 res = drop_unnamed_cols(res)
 res.to_excel(writer, sheet_name="联赛")
 
-res = df.groupby(['玩法分类']).sum()
+res1 = df[["投入", "盈利", "玩法分类"]].groupby(['玩法分类']).sum()
+res2 = df[["水位", "信心指数", "期望回报", "玩法分类"]].groupby(['玩法分类']).mean()
+res3 = df[["投入", "玩法分类"]].groupby(['玩法分类']).count().rename(columns={"投入": "次数"})
+res = res1.join(res2).join(res3)
 res["盈利比例"] = res["盈利"] / res["投入"]
 res = drop_unnamed_cols(res)
 res.to_excel(writer, sheet_name="玩法分类")
@@ -84,8 +93,11 @@ def over_under(x:str):
         return None
 
 df["大小球"] = df["玩法分类"].apply(lambda x: over_under(x))
-res = df.dropna(subset=["大小球"]).groupby(["大小球"]).sum()
-if not res.empty:
+df1 = df.dropna(subset=["大小球"])
+if not df1.empty:
+    res1 = df1[["投入", "盈利", "大小球"]].groupby(['大小球']).sum()
+    res2 = df1[["水位", "信心指数", "期望回报", "大小球"]].groupby(['大小球']).mean()
+    res = res1.join(res2)
     res["盈利比例"] = res["盈利"] / res["投入"]
     res_cnt = df.dropna(subset=["大小球"]).groupby(["大小球"]).count()["玩法分类"]
     res_cnt.name = "下注次数"
@@ -102,8 +114,11 @@ def asian_handicap(x:str):
         return None
 
 df["让球"] = df["玩法分类"].apply(lambda x: asian_handicap(x))
-res = df.dropna(subset=["让球"]).groupby(["让球"]).sum()
+df1 = df.dropna(subset=["让球"])
 if not res.empty:
+    res1 = df1[["投入", "盈利", "让球"]].groupby(['让球']).sum()
+    res2 = df1[["水位", "信心指数", "期望回报", "让球"]].groupby(['让球']).mean()
+    res = res1.join(res2)
     res["盈利比例"] = res["盈利"] / res["投入"]
     res_cnt = df.dropna(subset=["让球"]).groupby(["让球"]).count()["玩法分类"]
     res_cnt.name = "下注次数"
