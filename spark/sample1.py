ffa8a61592309bb9e386d2af6eca1a7636e4f283
josephchenhk <josephchenhk@gmail.com>
2023-04-02 10:14:27 +0800

    Add spark sample

---
+++ spark/sample1.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2/4/2023 8:33 am
+# @Author  : Joseph Chen
+# @Email   : josephchenhk@gmail.com
+# @FileName: sample1.py
+
+"""
+Copyright (C) 2022 Joseph Chen - All Rights Reserved
+You may use, distribute and modify this code under the terms of the JXW license,
+which unfortunately won't be written for another century.
+
+You should have received a copy of the JXW license with this file. If not,
+please write to: josephchenhk@gmail.com
+"""
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import isnan, when, count, col
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.clustering import KMeans
+
+# create a SparkSession
+spark = SparkSession.builder.appName('unsupervised_learning').getOrCreate()
+
+
+# Load data, type(df) = pyspark.sql.dataframe.DataFrame
+df = spark.read.csv("OHLCV.txt", header=True, inferSchema=True)
+
+# Remove date and ticker columns
+df = df.drop("date", "ticker")
+
+# Drop rows with NaN values
+df = df.na.drop()
+
+# Assemble features vector
+assembler = VectorAssembler(inputCols=df.columns, outputCol="features")
+# type(data) = pyspark.sql.dataframe.DataFrame
+data = assembler.transform(df)
+
+# Train KMeans model
+kmeans = KMeans(k=2, seed=1)
+model = kmeans.fit(data)
+
+# Make predictions
+# type(predictions) = pyspark.sql.dataframe.DataFrame
+predictions = model.transform(data)
+predictions.show()
